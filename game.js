e40e2a99ffa834499ac8ea4f0e37a11530fe1db2
antpowell <powell.anthony.e@gmail.com>
2019-11-02 22:25:45 -0500

    changing the 3D asset for testing

--- game.js
+++ game.js
@@ -2,21 +2,17 @@ import GIFExporter from 'babylonjs-gifexporter';
 
 const captureBtn = document.getElementById('captureBtn');
 
-BabylonViewer.viewerManager.onViewerAddedObservable.add(function (viewer) {
+BabylonViewer.viewerManager.onViewerAddedObservable.add(function(viewer) {
+	viewer.onEngineInitObservable.add(function(engine) {
+		const recorder = new GIFExporter(engine, {
+			delay: 50,
+			duration: 4000,
+		});
 
-    viewer.onEngineInitObservable.add(function (engine) {
-
-        const recorder = new GIFExporter(engine, {
-            delay: 50,
-            duration: 4000
-        });
-
-        captureBtn.addEventListener('click', function () {
-            captureBtn.disabled = true;
-            recorder.download('babylonjs.gif');
-            captureBtn.disabled = false;
-
-        })
-    });
-
+		captureBtn.addEventListener('click', function() {
+			captureBtn.disabled = true;
+			recorder.download('babylonjs.gif');
+			captureBtn.disabled = false;
+		});
+	});
 });
